cae0f0edf348805974a4eb737284f4fd95035fac
Ariiq Islam Alfajri <ariiq.i.alfajri@gmail.com>
2025-01-04 12:44:37 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary:'#64748b',
+        dark: '#0f172a', //hex warna
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
+
