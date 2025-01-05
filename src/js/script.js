7c0b4aab9134807f1fe1d1638134d690be6ecd03
Ariiq Islam Alfajri <ariiq.i.alfajri@gmail.com>
2025-01-05 20:25:11 +0700

    changing portfolio cta

--- src/js/script.js
+++ src/js/script.js
@@ -21,19 +21,3 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
-
-function truncateText(element, maxLength) {
-    const text = element.textContent;
-    const words = text.split(' ');
-    let truncatedText = '';
-
-    for (let i = 0; i < maxLength && i < words.length; i++) {
-        truncatedText += words[i] + ' ';
-    }
-
-    element.textContent = truncatedText.trim() + '...';
-}
-
-// Contoh penggunaan:
-const paragraph = document.querySelector('.paragraph');
-truncateText(paragraph, 150); // Batasi hingga berapa kata
