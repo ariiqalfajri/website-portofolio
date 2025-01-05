daccb2bb57532f6bf7d066fece0873a786eb125f
Ariiq Islam Alfajri <ariiq.i.alfajri@gmail.com>
2025-01-05 19:58:18 +0700

    updating something

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 // navbar fixed
-window.onscroll = function() {
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if(window.scrollY > fixedNav) {
+    if (window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
 
     } else {
@@ -17,7 +17,23 @@ const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
 
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+function truncateText(element, maxLength) {
+    const text = element.textContent;
+    const words = text.split(' ');
+    let truncatedText = '';
+
+    for (let i = 0; i < maxLength && i < words.length; i++) {
+        truncatedText += words[i] + ' ';
+    }
+
+    element.textContent = truncatedText.trim() + '...';
+}
+
+// Contoh penggunaan:
+const paragraph = document.querySelector('.paragraph');
+truncateText(paragraph, 150); // Batasi hingga berapa kata
